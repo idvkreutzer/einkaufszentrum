950d24fe96b45e1c6792ec1068cdfd7f9d66cb40
Maximilian Kreutzer <mkre@idv-ag.de>
2021-11-11 16:30:14 +0100

    init

---
+++ src/de/idv/einkaufszentrum/Einkaufszentrum.java
@@ -0,0 +1,10 @@
+package de.idv.einkaufszentrum;
+
+import de.idv.obstladen.Obstladen;
+
+public class Einkaufszentrum {
+
+	public static void main(String[] args) {
+		Obstladen.besuche();
+	}
+}
---
+++ src/module-info.java
@@ -0,0 +1,5 @@
+module app {
+	exports de.idv.einkaufszentrum;
+	
+	requires Obstladen;
+}
